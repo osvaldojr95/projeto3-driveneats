169f0f661405b3313f61f709420abdde2c88e77f
Osvaldo Júnior <osvaldojunior95@gmail.com>
2022-01-30 22:20:58 -0300

    Botão Finalizar pedido e tela de confirmação adicionado. Prompt com a requisição do nome e endereço adicionados. Arrumado erro do valor total

--- script.js
+++ script.js
@@ -1,32 +1,73 @@
+let prato = null;
+let bebida = null;
+let sobremesa = null;
+let nome = null;
+let endereco = null;
+
 function selecionando(categoria,produto){
     let sectionCategoria = document.querySelectorAll(".categoria");
-    let articleProdutos = sectionCategoria[categoria].querySelectorAll(".produto");
+    let jaSelecionado = sectionCategoria[categoria].querySelector(".selecionado");
+    let selecionado = sectionCategoria[categoria].querySelectorAll(".produto");
 
-    for(let i=0; i<3; i++){
-        let produtoUnico = articleProdutos[i];
-        let check = produtoUnico.querySelector("ion-icon");
-        if(i == produto){
-            produtoUnico.classList.add("selecionado");
-            check.classList.remove("escondido");
-        }
-        else{
-            produtoUnico.classList.remove("selecionado");
-            check.classList.add("escondido");
-        }
-    }
+    if(jaSelecionado !== null)
+        jaSelecionado.classList.remove("selecionado");
 
+    selecionado[produto].classList.add("selecionado");
+
+    let nome = selecionado[produto].querySelector("h3").innerHTML;
+    let valor = selecionado[produto].querySelector(".valor").innerHTML;
+
+    let biblio = {nome:nome,valor:parseFloat(valor.substring(3,valor.length).replace(",","."))}; 
+
+    if(categoria == 0)
+        prato = biblio;
+    else if(categoria == 1)
+        bebida = biblio;
+    else
+        sobremesa = biblio;
+    
     habilitarConfirmar();
 }
 
 function habilitarConfirmar(){
     let botao = document.querySelector(".botao");
     if(botao.classList.contains("desabilitado")){
-        let selecionados = document.querySelectorAll(".selecionado");
-        if(selecionados.length == 3){
-
+        if(prato!==null && bebida!==null && sobremesa!==null){
             botao.innerHTML = "Fechar pedido";
             botao.classList.remove("desabilitado");
             botao.classList.add("habilitado");
         }
     }
+}
+
+function fecharpedido(){
+    let telaConfirmacao = document.querySelector(".bg-confirmacao");
+
+    telaConfirmacao.querySelector(".prato-nome").innerHTML = prato.nome;
+    telaConfirmacao.querySelector(".prato-valor").innerHTML = prato.valor.toFixed(2).replace('.',',');
+    telaConfirmacao.querySelector(".bebida-nome").innerHTML = bebida.nome;
+    telaConfirmacao.querySelector(".bebida-valor").innerHTML = bebida.valor.toFixed(2).replace('.',',');
+    telaConfirmacao.querySelector(".sobremesa-nome").innerHTML = sobremesa.nome;
+    telaConfirmacao.querySelector(".sobremesa-valor").innerHTML = sobremesa.valor.toFixed(2).replace('.',',');
+    
+    let total = prato.valor + bebida.valor +sobremesa.valor;
+    telaConfirmacao.querySelector(".total-valor").innerHTML = `R$ ${total.toFixed(2).replace('.',',')}`;
+
+    telaConfirmacao.classList.remove("escondido");
+
+    obterDados();
+}
+
+function obterDados(){
+    nome = prompt("Insira seu nome:");
+    endereco = prompt("Insira seu endereco:");
+}
+
+function pedir(){
+
+}
+
+function cancelar(){
+    let telaConfirmacao = document.querySelector(".bg-confirmacao");
+    telaConfirmacao.classList.add("escondido");
 }
